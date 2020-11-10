aed27b0b01bb8d15f4a6fa36260462ce24e1d6f2
Sweta <71880635+SwetaPadmaja@users.noreply.github.com>
2020-11-10 18:39:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -41,9 +41,9 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
- paperObject.display();
+  paperObject.display();
   dustbinObj.display();
-  
+ 
   groundObject.display();
   
  
